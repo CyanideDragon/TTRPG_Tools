9e0a8840f1e377589660c56aea3a6a7e2fca756a
CyanideDragon <apachegoboom@gmail.com>
2023-01-16 11:57:35 -0700

    director fixed, maybe not final version

--- directing/director.py
+++ directing/director.py
@@ -1,48 +1,42 @@
 class Director:
-    '''The director of the game.
+    """A person who directs the game. 
     
-    The responsibility of the Director is to dictate and control the sequence of actions.
-    
-    Attributes:
-        _video_service (VideoService): Provides the GUI output.
+    The responsibility of a Director is to control the sequence of play.
 
-    '''
-    
+    Attributes:
+        _video_service (VideoService): For providing video output.
+    """
+
     def __init__(self, video_service):
-        '''Constructs a new director using the video service.
+        """Constructs a new Director using the specified video service.
         
         Args:
-            video_service (VideoService): an instance of the video service.'''
-        
+            video_service (VideoService): An instance of VideoService.
+        """
         self._video_service = video_service
-
-    def start_game(self):
-        '''Starts the game given cast and script, running the main game loop
         
-        args:
-            cast (Cast): the cast of actors.
-            script(Script): the script of actions'''
+    def start_game(self, cast, script):
+        """Starts the game using the given cast and script. Runs the main game loop.
 
-        ###### Cast and Script will be implemented in time. Delete once implemented.
-        
-        self._video_service.open_window() 
-        '''Calls to the video services open_window funciton'''
+        Args:
+            cast (Cast): The cast of actors.
+            script (Script): The script of actions.
+        """
+        self._video_service.open_window()
         while self._video_service.is_window_open():
-            self._video_service.clear_buffer()
-            self._video_service.flush_buffer()
-            #Clear and flush buffer must be removed and moved to a scripting action, such as drawing actors during a more complete implementation. 
-
+            self._execute_actions("input", cast, script)
+            self._execute_actions("update", cast, script)
+            self._execute_actions("output", cast, script)
         self._video_service.close_window()
-        '''Calls to the video services close_window funciton'''
-    
-    def _execute_actions(self):
-        '''calls execute for each action in the given group.
+
+    def _execute_actions(self, group, cast, script):
+        """Calls execute for each action in the given group.
         
         Args:
             group (string): The action group name.
-            cast (Cast): the cast of actors.
-            script (Script): the script of actors.
-            '''
-        ###### group, cas, script all need to be implemented in time. Delete once implemented.
-
-        pass
+            cast (Cast): The cast of actors.
+            script (Script): The script of actions.
+        """
+        actions = script.get_actions(group)    
+        for action in actions:
+            action.execute(cast, script)          
