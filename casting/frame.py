db9190767029c29d8b3f5c744938063be310771d
Jared Tart <jaredmtart@aol.com>
2023-01-20 16:19:36 -0700

    changed some comments

--- casting/frame.py
+++ casting/frame.py
@@ -4,20 +4,29 @@ import wx
 # a class for the frame
 class Frame(wx.Frame):
     def __init__(self, parent, title):
-        super(Frame, self).__init__(parent, # accesses the parent, in this case none as it is the frame
-        title=title,                        # sets the title to the one passed it
-        size=(960, 540),                    # sets the size in pixels (width, heigth)
-        style=wx.MAXIMIZE_BOX |             # draws the maximize box
-        wx.MINIMIZE_BOX |                   # draws the minimize box
-        wx.RESIZE_BORDER |                  # gives the ability for the user to resize the window
-        wx.SYSTEM_MENU |                    # gives a simple system menu
-        wx.CAPTION |	                    # gives the space for the above window functions
-        wx.CLOSE_BOX)                       # draws the close window box
+        """accesses the parent, in this case none as it is the frame; 
+        sets the title to the one passed it
+        sets the size in pixels (width, heigth); 
+        draws the maximize box; draws the minimize box; 
+        gives the ability for the user to resize the window; 
+        gives a simple system menu;
+        gives the space for the above window functions; 
+        draws the close window box"""
+        super(Frame, self).__init__(parent,  
+        title=title,                         
+        size=(960, 540),                     
+        style=wx.MAXIMIZE_BOX |              
+        wx.MINIMIZE_BOX |                    
+        wx.RESIZE_BORDER |                   
+        wx.SYSTEM_MENU |                     
+        wx.CAPTION |	                     
+        wx.CLOSE_BOX)                        
         
         self.InitUI()
 
     def InitUI(self):
-        self.Centre()                       # Sets the app to fullscreen upon opening
+        """ Sets the app to fullscreen upon opening """
+        self.Centre()
         self.menu()
 
     def menu(self):
@@ -30,7 +39,7 @@ class Frame(wx.Frame):
         self.abilitiesMenu = wx.Menu()
         self.statsMenu = wx.Menu()
 
-        #sets the menu bar to be reaedy to accept the menus
+        """sets the menu bar to be reaedy to accept the menus"""
         self.item = wx.MenuItem(self.fileMenu, 0, '&Check', helpString ="Check Help") 
         
         # add menus
