481e21dd3cb545ce1746c8b0709d0be82834b5a2
Jared Tart <jaredmtart@aol.com>
2023-01-23 15:56:16 -0700

    PUtting frame into f=different files

--- services/video_service.py
+++ services/video_service.py
@@ -1,2 +1,30 @@
-class VideoService:
-    pass
+import wx
+
+from casting.menu import Menu
+
+class VideoService(wx.Frame):
+    def __init__(self, parent, title):
+        """accesses the parent, in this case none as it is the frame; 
+        sets the title to the one passed it
+        sets the size in pixels (width, heigth); 
+        draws the maximize box; draws the minimize box; 
+        gives the ability for the user to resize the window; 
+        gives a simple system menu;
+        gives the space for the above window functions; 
+        draws the close window box"""
+        super().__init__(parent,  
+        title=title,                         
+        size=(960, 540),                     
+        style=wx.MAXIMIZE_BOX | 
+        wx.MINIMIZE_BOX | 
+        wx.RESIZE_BORDER | 
+        wx.SYSTEM_MENU | 
+        wx.CAPTION | 
+        wx.CLOSE_BOX)
+        
+        self.InitUI()
+
+    def InitUI(self):
+        """ Sets the app to fullscreen upon opening """
+        self.Centre()
+        Menu()
