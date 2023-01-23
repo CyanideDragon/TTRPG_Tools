ac4ae824b1b10587566e2b4e963fdb204e3adade
CyanideDragon <apachegoboom@gmail.com>
2023-01-23 15:53:34 -0700

    commented out some currently buggy files

--- services/keyboard_service.py
+++ services/keyboard_service.py
@@ -1,2 +1,14 @@
+import wx
+
 class KeyboardService:
-    pass
+    def __init__(self):
+        self.keys = {}
+
+    def is_key_up(self, key):
+        wx.EVT_KEY_UP()
+
+    def is_key_down(self, key):
+        wx.EVT_KEY_DOWN()
+
+    def is_key_held(self, key):
+        pass
