7d6d869fe63df12b3d691add584054afbe945cc5
Jared Tart <jaredmtart@aol.com>
2023-01-31 12:01:57 -0700

    Buttons now create menu options and then disapear

--- services/video_service.py
+++ services/video_service.py
@@ -30,29 +30,35 @@ class Frame(wx.Frame):
         wx.CAPTION | 
         wx.CLOSE_BOX)
 
-        # self.call()
+        """Centers the app on the screen"""
         self.Centre()
+        self.initial_menu(wx.EVT_BUTTON)
+        """Calls the function to make the initial screen"""
         self.choose_screen()
-        self.player_menu()
-        # self.menu.player_menu()
 
-        
-        
-    # def call(self):
-    #     self.Centre()
-    #     self.menu = Menu()
-    #     self.menu.player_menu()
-
-    def player_menu(self):
-        """Creates and adds menu items"""
+    """Created a file menu that only have the quit option"""
+    def initial_menu(self, event):
         self.menuBar = wx.MenuBar()
         self.fileMenu = wx.Menu()
+        self.menuBar.Append(self.fileMenu, '&File')
+        self.fileQuit = wx.MenuItem(self.fileMenu, wx.ID_EXIT, '&Quit\tCtrl+Q') 
+        self.Bind(wx.EVT_MENU, self.OnQuit, self.fileQuit)
+        self.fileMenu.Append(self.fileQuit)
+        
+        self.SetMenuBar(self.menuBar)
+        self.Show(True)
+        
+    """Creates a menu with all of the options for players"""
+    def player_menu(self, event):
+        """Hides the screen select buttons"""
+        self.player_button.Hide()
+        self.DM_button.Hide()
+
+        """Creates and adds menu items"""
         self.characterMenu = wx.Menu()
         self.actionMenu = wx.Menu()     
         self.abilitiesMenu = wx.Menu()
         self.statsMenu = wx.Menu()
-        self.item = wx.MenuItem(self.fileMenu, 0, '&Check', helpString ="Check Help")
-        self.menuBar.Append(self.fileMenu, '&File')
         self.menuBar.Insert(1, self.actionMenu, '&Actions')
         self.menuBar.Insert(1, self.abilitiesMenu, '&Abilities')
         self.menuBar.Insert(1, self.statsMenu, '&Stats')
@@ -65,23 +71,34 @@ class Frame(wx.Frame):
         characters.Append(wx.ID_ANY, 'Moe')
         self.characterMenu.AppendMenu(wx.ID_ANY, '&Characters', characters)
 
-        self.fileQuit = wx.MenuItem(self.fileMenu, wx.ID_EXIT, '&Quit\tCtrl+Q') 
-        self.Bind(wx.EVT_MENU, self.OnQuit, self.fileQuit)
-        self.fileMenu.Append(self.fileQuit)
-
         """Draws the menu Bar"""
         self.SetMenuBar(self.menuBar)
         self.Show(True)
+
+    """Creates a menu with all of the options for players"""
+    def DM_menu(self, event):
+        """Hides the screen select buttons"""
+        self.player_button.Hide()
+        self.DM_button.Hide()
         
+    """Creates the buttons that """
     def choose_screen(self):
         """Creates the DM / Player select buttons"""
+        font = wx.Font(18, wx.MODERN, wx.NORMAL, wx.NORMAL)
+
+        """Creates the DM / Player select buttons"""
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        player_button = wx.Button(self, label='Player', pos=(600,150), size=(150,150))
-        DM_button = wx.Button(self, label='Dungeon Master', pos=(160,150), size=(150,150))
-        main_sizer.Add(DM_button, 1, wx.Left, 0)
-        main_sizer.Add(player_button, 2, wx.Right, 0)
-        
+        self.player_button = wx.Button(self, label='Player', pos=(600,150), size=(200,200))
+        self.DM_button = wx.Button(self, label='Dungeon\nMaster', pos=(160,150), size=(200,200))
+        self.player_button.SetFont(font)
+        self.DM_button.SetFont(font)
+        main_sizer.Add(self.DM_button, 1, wx.Left, 0)
+        main_sizer.Add(self.player_button, 2, wx.Right, 0)
 
+        """Makes it so the buttons will react when clicked"""
+        self.Bind(wx.EVT_BUTTON, self.player_menu, self.player_button)
+        self.Bind(wx.EVT_BUTTON, self.DM_menu, self.DM_button)
 
+    """Closes the app when called"""
     def OnQuit(self, e):
         self.Close(True)
