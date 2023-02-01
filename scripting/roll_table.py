1a2e5dc58d9df3e5982973acb9675d23e7284403
Stryker-Blade <jensen.d.call@gmail.com>
2023-01-31 22:46:50 -0700

    Added some basics in
    Added a bit of simple code in, still got a lot to work on.

--- scripting/roll_table.py
+++ scripting/roll_table.py
@@ -1,5 +1,13 @@
 from scripting.action import Action
+import random
 
 class RollTable(Action):
     def __init__(self):
         pass
+    current_roll_tables= {[0, "Tavern"], [1, "Weather"], [2, "Boat travel"] , [3, "Travel on horseback"], [4, "Severe weather"], [5, "Monster attack"]}
+    print(f"{current_roll_tables}")
+    user_input = input("What kind of roll table do you want 1-10")
+    selected_table = current_roll_tables[user_input]
+    def execute(self, dice_type):
+        dice_roll = random.randint(1, dice_type)
+        return super().execute()
