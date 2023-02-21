3ca179de1c188445a176072d1d17ffe71b24adbb
CyanideDragon <apachegoboom@gmail.com>
2023-02-21 15:39:53 -0700

    fixed pathing mismathc

--- casting/map.py
+++ casting/map.py
@@ -7,7 +7,7 @@ class Map:
 
     def display_map(self):
         """Creates the image for the map and displays it on the screen"""
-        map_file = 'casting\map.png'
+        map_file = 'images\map.png'
         map = wx.Image(map_file, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         self.bitmap1 = wx.StaticBitmap(self, -1, map, (5,5))
         map_border = wx.StaticBox(self, wx.ID_ANY, size=(996,650), pos=(5,5))
